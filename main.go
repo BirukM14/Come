0c98b6a4eee8bbbffa42f85985abb3694fa41d92
BirukM14 <gbiruka14@gmail.com>
2025-02-14 09:23:31 +0300

    implement main function to start HTTP server and handle routes

--- main.go
+++ main.go
@@ -5,3 +5,16 @@ import (
 	"log"
 	"net/http"
 )	
+
+
+func main() {
+	fileServer := http.FileServer(http.Dir("./static"))
+	http.Handle("/", fileServer)
+	http.HandleFunc("/form", formHandler)
+	http.HandleFunc("/hello", helloHandler)
+
+	fmt.Printf("Starting server on :8080\n")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
+}
