d54bbddc32b5483028c79bacea9d192b25da2372
BirukM14 <gbiruka14@gmail.com>
2025-02-14 09:42:45 +0300

    rename formHandler to helloHandler and enhance formHandler to process user input

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 	"net/http"
 )	
 
-func formHandler(w http.ResponseWriter, r *http.Request) {
+func helloHandler(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/hello" {
 		http.Error(w, "404 not found.", http.StatusNotFound)
 		return
@@ -16,6 +16,20 @@ func formHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	fmt.Fprintf(w, "Hello!")
+}
+
+func formHandler(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "ParseForm() err: %v", err)
+		return
+	}
+	
+	fmt.Fprintf(w, "successful post request!")
+	name := r.FormValue("name")
+	address := r.FormValue("address")
+	fmt.Fprintf(w, "Name = %s\n", name)
+	fmt.Fprintf(w, "Address = %s\n", address)
+	
 }
 
 func main() {
